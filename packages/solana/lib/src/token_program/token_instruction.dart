a7609d3a3c7d36dec61fb17e542e6d7de55a1db4
soonwoo <soonwoo.dev@gmail.com>
2021-12-01 17:57:03 +0900

    add newOrder

--- packages/solana/lib/src/token_program/token_instruction.dart
+++ packages/solana/lib/src/token_program/token_instruction.dart
@@ -39,10 +39,7 @@ class TokenInstruction extends Instruction {
           Buffer.fromUint8(decimals),
           Buffer.fromBase58(mintAuthority),
           Buffer.fromUint8(freezeAuthority != null ? 1 : 0),
-          if (freezeAuthority != null)
-            Buffer.fromBase58(freezeAuthority)
-          else
-            List<int>.filled(32, 0),
+          if (freezeAuthority != null) Buffer.fromBase58(freezeAuthority) else List<int>.filled(32, 0),
         ]),
       );
 
@@ -86,6 +83,59 @@ class TokenInstruction extends Instruction {
           Buffer.fromUint64(amount),
         ]),
       );
+
+  factory TokenInstruction.newOrder({
+    required String market,
+    required String openOrders,
+    required String payer,
+    required String owner,
+    required String requestQueue,
+    required String baseVault,
+    required String quoteVault,
+    required String side,
+    required int limitPrice,
+    required int maxQuantity,
+    required String programId,
+    required String orderType,
+    String? clientId,
+    String? feeDiscountPubkey,
+  }) {
+    final List<AccountMeta> keys = [
+      AccountMeta(pubKey: market, isSigner: false, isWriteable: true),
+      AccountMeta(pubKey: openOrders, isSigner: false, isWriteable: true),
+      AccountMeta(pubKey: requestQueue, isSigner: false, isWriteable: true),
+      AccountMeta(pubKey: payer, isSigner: false, isWriteable: true),
+      AccountMeta(pubKey: owner, isSigner: true, isWriteable: false),
+      AccountMeta(pubKey: baseVault, isSigner: false, isWriteable: true),
+      AccountMeta(pubKey: quoteVault, isSigner: false, isWriteable: true),
+      AccountMeta(pubKey: programId, isSigner: false, isWriteable: false),
+      AccountMeta(pubKey: Sysvar.rent, isSigner: false, isWriteable: false),
+    ];
+    if (feeDiscountPubkey != null) {
+      keys.add(AccountMeta(pubKey: feeDiscountPubkey, isSigner: false, isWriteable: false));
+    }
+
+    final List<Buffer> bufferList = clientId != null
+        ? [
+            Buffer.fromString(side),
+            Buffer.fromUint64(limitPrice),
+            Buffer.fromUint64(maxQuantity),
+            Buffer.fromString(orderType),
+            Buffer.fromString(clientId),
+          ]
+        : [
+            Buffer.fromString(side),
+            Buffer.fromUint64(limitPrice),
+            Buffer.fromUint64(maxQuantity),
+            Buffer.fromString(orderType),
+          ];
+    return TokenInstruction._(
+      accounts: keys,
+      data: Buffer.fromConcatenatedByteArrays([
+        ...bufferList,
+      ]),
+    );
+  }
 
   /// Mint the [destination] account with [amount] tokens of the [mint] token.
   /// The [authority] is the mint authority of the token.
