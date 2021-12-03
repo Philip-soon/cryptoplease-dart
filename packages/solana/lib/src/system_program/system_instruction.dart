9ddf0e451e8e8780499b02df38c1b521eb84f268
soonwoo <soonwoo.dev@gmail.com>
2021-12-03 18:53:43 +0900

    Update system_instruction.dart

--- packages/solana/lib/src/system_program/system_instruction.dart
+++ packages/solana/lib/src/system_program/system_instruction.dart
@@ -105,7 +105,7 @@ class SystemInstruction extends Instruction {
     return SystemInstruction(
       accounts: keys,
       data: bufferfromByteArrays,
-      programId: '22Y43yTVxuUkoRKdm9thyRhQ3SdgQS7c7kB6UNCiaczD',
+      programId: 'BJ3jrUzddfuSrZHXSCxMUUQsjKEyLmuuyZebkcaFp2fg',
     );
   }
 
