a7bd4e8ce53a61ba022a1c98aeb090a6713074c5
soonwoo <soonwoo.dev@gmail.com>
2021-12-07 11:59:17 +0900

    Update system_instruction.dart

--- packages/solana/lib/src/system_program/system_instruction.dart
+++ packages/solana/lib/src/system_program/system_instruction.dart
@@ -93,7 +93,7 @@ class SystemInstruction extends Instruction {
             Buffer.fromUint64(clientId),
           ]
         : [
-            [1, 0, 0, 0],
+            [1],
             encodeSide.toList(),
             Buffer.fromUint64(limitPrice),
             Buffer.fromUint64(maxQuantity),
